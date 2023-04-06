d55ad39cf9db0330861f570630f6e897b23f3a18
ibrahim3999 <mgdlhb@gmail.com>
2023-04-07 00:28:17 +0300

    testUpdate

--- StudentTest1.cpp
+++ StudentTest1.cpp
@@ -4,9 +4,7 @@
 #include "sources/game.hpp"
 #include "sources/card.hpp"
 using namespace std;
-using  namespace ariel;
-
-
+using namespace ariel;
 
 TEST_CASE("The amount of cards before starting a game")
 {
@@ -67,7 +65,6 @@ TEST_CASE("Activating another turn after the game is over")
     CHECK_THROWS(game.playTurn());
 }
 
-
 TEST_CASE("One player")
 {
     Player p1("Alice");
@@ -82,29 +79,21 @@ TEST_CASE("Printing the winner")
     Game game(p1, p2);
     game.playAll();
 
-    if (p1.cardesTaken() == p2.cardesTaken())
-    {
-        CHECK_THROWS(game.printWiner());
-    }
-    else
-    {
-        CHECK_NOTHROW(game.printWiner());
-    }
+    CHECK_NOTHROW(game.printWiner());
 }
-/**
+
 TEST_CASE("The game ends after at most 26 turns")
 {
     Player p1("Alice");
     Player p2("Bob");
     Game game(p1, p2);
-    bool maxTurns = 26;
-    
-    for (int i = 0 ; i <= 26 ; i++ && p1.stacksize()>=0)
+    int maxTurns = 26;
+    int i = 0;
+    for ( ; i < 26 && p1.stacksize()>0; i++ )
     {
         game.playTurn();
     }
-    CHECK(maxTurns == 26);
+    CHECK(maxTurns >= i );
     CHECK(p1.stacksize() == 0);
     CHECK(p2.stacksize() == 0);
 }
-*/
